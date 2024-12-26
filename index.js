26ef02bdba0c24ae950d3ee6cfc241a217a58b08
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2024-12-26 15:17:29 -0300

    feat: DIST-6576 initial structure

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import axios from "axios;";
+// import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+const API_URL = "http://api.currencylayer.com/";
+const API_KEY = "0d402cae541b11d6ba5af58cc9f8585b";
+
+app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
