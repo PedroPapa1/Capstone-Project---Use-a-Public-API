706ee59cf41d7e66747c7f265195ba8e3bae43c7
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2025-01-28 12:46:17 -0300

    fix: remove the duplicate codes / add error handling / add box shadow

--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 import express from "express";
-import axios from "axios";
 import bodyParser from "body-parser";
+import { getTopExchangeRates } from "./service/currencyService.js";
 
 const app = express();
 const port = 3000;
-
-const API_URL = "http://api.currencylayer.com";
-const API_KEY = "0d402cae541b11d6ba5af58cc9f8585b";
-const TOP_CURRENCIES = ["USD", "EUR", "JPY", "GBP", "CHF", "CAD", "AUD", "ZAR", "CNY", "BRL"];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
-  const sourceCurrency = req.params.currency;
-  const topCurrencies = TOP_CURRENCIES.filter((topCurrency) => topCurrency !== sourceCurrency).join(",");
+  try {
+    const sourceCurrency = req.params.currency;
+    const exchangeRates = await getTopExchangeRates(sourceCurrency);
 
-  const exchangeRatesResponse = await axios.get(`${API_URL}/live`, {
-    params: {
-      access_key: API_KEY,
-      source: sourceCurrency,
-      currencies: topCurrencies,
-    },
-  });
-
-  const exchangeRates = Object.entries(exchangeRatesResponse.data.quotes).map(([key, value]) => {
-    const formattedKey = `${key.slice(0, 3)} -> ${key.slice(3)}`;
-    return `${formattedKey}: ${value}`;
-  });
-
-  res.render("index.ejs", { exchangeRates });
+    res.render("index.ejs", { exchangeRates });
+  } catch (error) {
+    console.error("Error fetching exchange rates:", error);
+    res.status(404).send(error.message);
+  }
 });
 
 app.listen(port, () => {
--- service/currencyService.js
+++ service/currencyService.js
@@ -1,26 +1,28 @@
-import axios, { all } from "axios";
+import axios from "axios";
 
 const API_URL = "http://api.currencylayer.com/";
-const API_KEY = "742c4de70e76d2d66101128577085452";
+const API_KEY = "65d96383658e1a054897c95ad6c4be85";
 const TOP_CURRENCIES = ["USD", "EUR", "JPY", "GBP", "CHF", "CAD", "AUD", "ZAR", "CNY", "BRL"];
-
-let allCurrencies;
 
 export async function getTopExchangeRates(sourceCurrency) {
   const topCurrencies = TOP_CURRENCIES.filter((topCurrency) => topCurrency !== sourceCurrency).join(",");
 
-  const exchangeRatesResponse = await axios.get(`${API_URL}/live`, {
-    params: {
-      access_key: API_KEY,
-      source: sourceCurrency,
-      currencies: topCurrencies,
-    },
-  });
+  try {
+    const exchangeRatesResponse = await axios.get(`${API_URL}/live`, {
+      params: {
+        access_key: API_KEY,
+        source: sourceCurrency,
+        currencies: topCurrencies,
+      },
+    });
 
-  const exchangeRates = Object.entries(exchangeRatesResponse.data.quotes).map(([key, value]) => {
-    const formattedKey = `${key.slice(0, 3)} -> ${key.slice(3)}`;
-    return `${formattedKey}: ${value}`;
-  });
+    const exchangeRates = Object.entries(exchangeRatesResponse.data.quotes).map(([key, value]) => {
+      const formattedKey = `${key.slice(0, 3)} -> ${key.slice(3)}`;
+      return `${formattedKey}: ${value}`;
+    });
 
-  return exchangeRates;
+    return exchangeRates;
+  } catch (error) {
+    console.log(`Error fetching exchange rates:${error}`);
+  }
 }
