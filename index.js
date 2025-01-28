0edd406d38163aa6211e956a28750cab32747938
PedroPapaCloudbeds <pedro.papa@cloudbeds.com>
2025-01-28 14:50:30 -0300

    fix: remove duplicates const

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-const API_URL = "http://api.currencylayer.com/";
-const API_KEY = "0d402cae541b11d6ba5af58cc9f8585b";
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
